6e1d8a186f273b9d7ff11895a7a2891dfb220817
Silas Franca <silasfranka@hotmail.com>
2023-08-12 20:52:34 -0300

    md

--- exe1/controller1/somatorio.java
+++ exe1/controller1/somatorio.java
@@ -1,31 +1,23 @@
-package controller;
-
+package controller1;
 
 public class somatorio {
 
 	public somatorio() {
 		super();
-	}	
-	
+	}
 	
 	public double nnaturais(int n,double[] vetor, double soma) {
-		 
-		
+			 
 		if (n == 0) {
-			
-		return soma;
+				
+			return soma;
 		} else {
 			if(vetor[n-1]>=0) {
-				
 			soma = soma + vetor[n-1];	
-				
-			}
-			
 		}
-		
+				
 		return nnaturais(n-1,vetor, soma);
-	}
-}	
-	
+				}
+	}	
 
-	
+}
--- exe1/view1/Principal.java
+++ exe1/view1/Principal.java
@@ -1,15 +1,17 @@
-package view;
-import javax.swing.JOptionPane;
-import controller.somatorio;
+package view1;
 
-public class principal {
+import javax.swing.JOptionPane;
+
+import controller1.somatorio;
+
+public class Principal {
 
 	public static void main(String[] args) {
 		double vetor[] = {-2,5,2,7,-9,-8};
 		int n;
 		double soma =0 ;
 		
-		n = Integer.parseInt(JOptionPane.showInputDialog("Informe at� quantos n�emros ( at� 6"));
+		n = Integer.parseInt(JOptionPane.showInputDialog("Informe ate quantos numeros ( ate 6"));
 		somatorio c = new somatorio();
 		
 		double res = c.nnaturais(n, vetor,soma);
