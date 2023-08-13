e1d80afe81f4132f0d24c68e749458020cdac639
Silas Franca <silasfranka@hotmail.com>
2023-08-13 00:00:20 -0300

    md

--- exe5/controller5/soma.java
+++ exe5/controller5/soma.java
@@ -6,11 +6,11 @@ public class soma {
 		super();
 	}
 	public double SomaDouble(double n) {
-		
+		// para quando n chega a zero
 		if (n==0) {
 			return 0;
 		}
-		
+		//quando retorna divide 1 por n retornando
 		return  (SomaDouble(n-1)+ (1/(n)));
 	}
 
